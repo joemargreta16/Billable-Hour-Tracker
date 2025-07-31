1e50c043ec958777394d372712c0318c42f18d3c
joemargreta16 <55623871+joemargreta16@users.noreply.github.com>
2025-07-31 18:43:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def init_db_command():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
